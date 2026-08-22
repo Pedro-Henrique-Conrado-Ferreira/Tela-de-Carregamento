4544d69f72fb197441632f54ad8757b3aa9eca33
Pedro Henrique Conrado Ferreira <pudrink365@gmail.com>
2026-08-22 09:59:57 -0300

    Improve layout and image in loading screen
    Refactor loading screen layout and update image source.

--- Tela-De-Carregamento.jsx
+++ Tela-De-Carregamento.jsx
@@ -1,27 +1,29 @@
-#Tela de Carregamento
-import { Image, Text, View, TextInput } from 'react-native';
-
+import { Image, Text, View } from 'react-native';
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor:'#2C3E35'}}>
-      <View style={{ alignItems: 'center',paddingTop:25 }}>
-        <Text style={{ fontSize: 24, fontWeight: 'bold',color: 'white' }}>Boca de Planta</Text>
-      </View>
-      <View style={{ alignItems: 'center' }}>
-        <Text style={{ fontSize: 18,color: 'white'  }}> Onde a natureza mostra os dentes </Text>
-      </View>
-      <View style={{ alignItems: 'center' }}>
+    <View style={{ flex: 1, backgroundColor: '#2C3E35' }}>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Image
-          source={{
-            uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRk7IERwRRtLAZct3fDSAipAmNBV31tC48BCQ&s',
-          }}
-          style={{ width: 120, height: 150, margin: 130, padding: 10 }}
+          source={require('./assets/images__2_-removebg-preview.png')}
+          style={{ width: 120, height: 150, marginBottom: 20 }}
         />
+        <Text
+          style={{
+            fontSize: 24,
+            fontWeight: 'bold',
+            color: 'white',
+            marginBottom: 8,
+          }}>
+          Boca de Planta
+        </Text>
+        <Text style={{ fontSize: 18, color: 'white', textAlign: 'center' }}>
+          Onde a natureza mostra os dentes
+        </Text>
       </View>
       <View
         style={{
           height: 60,
-          backgroundColor: '#',
+          backgroundColor: '#1E2B25',
           justifyContent: 'center',
           alignItems: 'center',
           borderTopWidth: 1,
